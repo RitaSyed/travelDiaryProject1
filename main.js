2f4c01c642c1884b194941b48ae8b6ce0f01dd55
Rita Patrick <rita.patrick1007@gmail.com>
2018-03-24 17:55:00 -0500

    added comments for code, added time function from buildCardOutput function's code

--- main.js
+++ main.js
@@ -31,11 +31,13 @@ const travelPlaces = [
     }
 ];
 
+//prints all the cards and cards entries to DOM
 const PrintToDom = (domString, divId) => {
   const PrintTo = document.getElementById(divId);
   PrintTo.innerHTML += domString;
 };
 
+//builds each card from travelPlaces array of objects
 const buildDomString = (places) => {
   let domString = "";
   places.forEach((place) => {
@@ -50,6 +52,7 @@ const buildDomString = (places) => {
   PrintToDom(domString, "card-holder");
 }
 
+//adds event listeners for all the buttons on the page
 const addAllEventListeners = () =>{
   const cardHolderDiv = document.getElementById("card-holder");
   const mainOutput = document.getElementById("main-output");
@@ -70,6 +73,7 @@ const addAllEventListeners = () =>{
     });
 }
 
+//changes card's background and button colors when clicked submit
 const changeCardToGreen = (e) => {
   let card = e.target.parentNode;
   let button = e.target;
@@ -79,27 +83,36 @@ const changeCardToGreen = (e) => {
   }
 };
 
+//shows date of each diary card entry
+const timestamp = (e) => {
+   let time = new Date();
+   return time;
+}
+
+
+//builds diary card entries
 const buildCardOutput = (e) => {
   let input = e.target.previousElementSibling.value;
   e.target.previousElementSibling.value="";
-  let time = new Date();
   let title = e.target.parentNode.firstElementChild.textContent;
   let string = "";
   string += `<div id="card-output">`;
   string +=    `<h2>${title}</h1>`;
   string +=    `<p class="card-entry">${input}</p>`;
-  string +=    `<p>${time}</p>`;
+  string +=    `<p>${timestamp()}</p>`;
   string +=     `<button class="edit-button">Edit</button>`;
   string +=     `<button class="delete-button">Delete</button>`;
   string += `</div>`;
   PrintToDom(string, "main-output");
 };
 
+//removes card entry using delete button
 const removesDiaryEntry = (e, mainOutput) => {
   let cardOutput = e.target.parentNode;
   mainOutput.removeChild(cardOutput);
   }
 
+//edits card entry using edit button
 const editDiaryEntry = (e) => {
   let cardOutput = e.target.parentNode;
   let cardEntry = cardOutput.children[1];
